5c2cc47e8f54084175af2102ec3f4e7270a00447
AMwolf9000 <169104276+AMwolf9000@users.noreply.github.com>
2025-12-03 15:51:26 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /* TODO:
-    mutaionObserver for new elements
+    go into shadow roots/iframes
+    ::before & ::after ??
 */
 (async () => {
 // add style sheet before waiting for DOM to prevent white flash from loading
@@ -13,10 +14,10 @@ const style = document.createElement("style");
 style.textContent = overideBasicStyleSheet;
 document.head.appendChild(style);
 
-// wait for some DOM
-await new Promise(resolve => setTimeout(resolve, 100));
-
 overideBasicStyleSheet = `
+* {
+    scrollbar-color: rgb(42, 42, 42) rgb(106, 106, 106) !important;
+}
 *:not(a) {
     color: white !important;
 }
@@ -28,6 +29,12 @@ img, video {
 }
 a {
     color: rgb(0, 0, 255) !important;
+}
+.background-image-initial {
+    background-image: initial !important;
+}
+.background-filter-brightness {
+    filter: brightness(0.7) !important;
 }
 .background-black {
     background-color: black !important;
@@ -60,24 +67,60 @@ a {
     background-color: rgba(0, 0, 0, 0.9) !important;
 }
 `;
-const elems = document.body.querySelectorAll("*");
-elems.forEach(resolveClassForElems);
 
-function resolveClassForElems(element, index, array) {
-    const bgCol = window.getComputedStyle(element).backgroundColor;
-    const alpha = parseFloat(bgCol.split(",")[3]);
-    console.log(bgCol, alpha);
-    if (alpha == 0) {
-        return;
-    } else if (isNaN(alpha)) {
-        element.classList.add("background-black");
-    } else {
-        const ar = alpha.toPrecision(1).toString().replace(".", "_");
-        console.log(ar);
-        element.classList.add(`background-trans${ar}`);
+// wait for some DOM
+await new Promise(resolve => setTimeout(resolve, 100));
+
+const elems = document.body.querySelectorAll("*");
+// disable styleSheet to prevent interference from inital style sheet
+style.disabled = true
+
+resolveClassForElems(elems)
+
+function resolveClassForElems(elementList, index, array) {
+    console.log(elementList)
+    for (const element of elementList) {
+        const computedStyle = window.getComputedStyle(element)
+        const bgCol = computedStyle.backgroundColor;
+        const bgImg = computedStyle.backgroundImage;
+        const alpha = parseFloat(bgCol.split(",")[3]);
+        console.log(bgCol, bgImg);
+
+        // handle background-image
+        if (bgImg != "none") {
+            if (bgImg.includes("url")) {
+                element.classList.add("background-filter-brightness");
+            } else {
+                element.classList.add("background-image-initial");
+            }
+        }
+
+        // handle background-color
+        if (alpha == 0) {
+            continue;
+        } else if (isNaN(alpha)) {
+            element.classList.add("background-black");
+        } else {
+            const ar = alpha.toPrecision(1).toString().replace(".", "_");
+            element.classList.add(`background-trans${ar}`);
+        }
     }
-} 
+}
 
 style.textContent = overideBasicStyleSheet;
+style.disabled = false
 
+const observer = new MutationObserver(mutationList => {
+    for (const mutationRecord of mutationList) {
+        if (mutationRecord.addedNodes.length == 0) continue;
+
+        for (const node of mutationRecord.addedNodes) {
+            if (node.nodeType != Node.ELEMENT_NODE) continue;
+
+            resolveClassForElems([node, ...node.querySelectorAll("*")])
+        }
+    }
+})
+
+observer.observe(document.body, {subtree: true, childList: true})
 })();
